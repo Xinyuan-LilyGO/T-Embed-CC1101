1b650269518175d5009ccef0715d290330e95dd4
ShallowGreen123 <2608653986@qq.com>
2025-02-06 11:02:23 +0800

    add shutdown example

---
+++ examples/bq25896_shutdown/bq25896_shutdown.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include <XPowersLib.h>
+#include "utilities.h"
+
+XPowersPPM PPM;
+
+const uint8_t i2c_sda = BOARD_I2C_SDA;
+const uint8_t i2c_scl = BOARD_I2C_SCL;
+const uint8_t pmu_irq_pin = CONFIG_PMU_IRQ;
+uint32_t cycleInterval;
+uint32_t countdown = 10;
+
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial);
+
+    bool result =  PPM.init(Wire, i2c_sda, i2c_scl, BQ25896_SLAVE_ADDRESS);
+    if (result == false) {
+        while (1) {
+            Serial.println("PPM is not online...");
+            delay(1000);
+        }
+    }
+
+}
+
+
+void loop()
+{
+    if (millis() > cycleInterval) {
+        Serial.printf("%d\n", countdown);
+        if (!(countdown--)) {
+            Serial.println("Shutdown .....");
+            // The shutdown function can only be used when the battery is connected alone,
+            // and cannot be shut down when connected to USB.
+            // It can only be powered on in the following two ways:
+            // 1. Press the PPM/QON button
+            // 2. Connect to USB
+            PPM.shutdown();
+            countdown = 10000;
+        }
+        cycleInterval = millis() + 1000;
+    }
+}
+
+
+
+
+
---
+++ examples/bq25896_shutdown/utilities.h
@@ -0,0 +1,70 @@
+
+
+#pragma once
+
+// WS2812
+#define WS2812_NUM_LEDS 8
+#define WS2812_DATA_PIN 14
+
+// IR
+#define BOARD_IR_EN 2
+#define BOARD_IR_RX 1
+
+// MIC
+#define BOARD_MIC_DATA 42
+#define BOARD_MIC_CLK  39
+
+// --------- DISPLAY ---------
+// About LCD definition in the file: lib/TFT_eSPI/User_Setups/Setup214_LilyGo_T_Embed_PN532.h
+// #define ST7789_DRIVER     // Configure all registers
+// #define TFT_WIDTH  170
+// #define TFT_HEIGHT 320
+
+// #define TFT_BL     21   // LED back-light
+// #define TFT_MISO   -1   
+// #define TFT_MOSI   9
+// #define TFT_SCLK   11
+// #define TFT_CS     41 
+// #define TFT_DC     10
+// #define TFT_RST    40 // Connect reset to ensure display initialises
+
+
+// --------- ENCODER ---------
+#define ENCODER_INA 4
+#define ENCODER_INB 5
+#define ENCODER_KEY 0
+
+// --------- IIC ---------
+#define BOARD_I2C_SDA  8
+#define BOARD_I2C_SCL  18
+
+// IIC addr
+#define BOARD_I2C_ADDR_1 0x24  // PN532  
+#define BOARD_I2C_ADDR_2 0x55  // PMU   
+#define BOARD_I2C_ADDR_3 0x6b  // BQ25896
+
+#define BOARD_PN532_SCL     BOARD_I2C_SCL
+#define BOARD_PN532_SDA     BOARD_I2C_SDA
+#define BOARD_PN532_RF_REST 45
+#define BOARD_PN532_IRQ     17
+
+// --------- SPI ---------
+#define BOARD_SPI_SCK  11
+#define BOARD_SPI_MOSI 9
+#define BOARD_SPI_MISO 10
+
+// TF card
+#define BOARD_SD_CS   13
+#define BOARD_SD_SCK  BOARD_SPI_SCK
+#define BOARD_SD_MOSI BOARD_SPI_MOSI
+#define BOARD_SD_MISO BOARD_SPI_MISO
+
+// LORA
+#define BOARD_LORA_CS   12
+#define BOARD_LORA_SCK  BOARD_SPI_SCK
+#define BOARD_LORA_MOSI BOARD_SPI_MOSI
+#define BOARD_LORA_MISO BOARD_SPI_MISO
+#define BOARD_LORA_IO2  38
+#define BOARD_LORA_IO0  3
+#define BOARD_LORA_SW1  47
+#define BOARD_LORA_SW0  48
