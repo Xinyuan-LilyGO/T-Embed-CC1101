6d4346caabfb16f0d8a1f241906c2621d9157389
ShallowGreen123 <2608653986@qq.com>
2025-08-07 16:10:12 +0800

    Update BTN test

--- examples/encode_test/encode_test.ino
+++ examples/encode_test/encode_test.ino
@@ -3,24 +3,11 @@
 #define ENCODER_INA 4
 #define ENCODER_INB 5
 #define ENCODER_KEY 0
+#define BOARD_USER_KEY 6
 
 // About LCD definition in the file: lib/TFT_eSPI/User_Setups/Setup214_LilyGo_T_Embed_PN532.h
 TFT_eSPI tft;
 RotaryEncoder encoder(ENCODER_INA, ENCODER_INB, RotaryEncoder::LatchMode::TWO03);
-
-int display_rotation = 3;
-
-void encoder_handler(void)
-{
-    if(display_rotation == 3){
-        tft.setRotation(1);
-        display_rotation = 1;
-    } else if(display_rotation == 1){
-        tft.setRotation(3);
-        display_rotation = 3;
-    }
-    Serial.printf("display_rotation = %d\n", display_rotation);
-}
 
 void setup(void)
 {
@@ -36,12 +23,12 @@ void setup(void)
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.setTextSize(7);
-    tft.setCursor(150, 60); 
-    tft.print("0");
+    tft.setTextSize(2);
+    tft.setCursor(50, 40); 
+    tft.print("encode = 0     ");
 
     pinMode(ENCODER_KEY, INPUT);
-    attachInterrupt(ENCODER_KEY, encoder_handler, FALLING);
+    pinMode(BOARD_USER_KEY, INPUT);
 }
 
 void loop(void)
@@ -50,17 +37,43 @@ void loop(void)
     encoder.tick();
 
     int newPos = encoder.getPosition();
+
     if (pos != newPos) {
-        tft.fillScreen(TFT_BLACK);
+        // tft.fillScreen(TFT_BLACK);
         String str = String(newPos);
         int txt_width = tft.textWidth(str.c_str());
         int scr_width = tft.width();
-        tft.setCursor((scr_width - txt_width) / 2, 60); 
-        tft.print(newPos);
-        tft.print(" ");
+        // tft.setCursor((scr_width - txt_width) / 2, 60); 
+        // tft.print(newPos);
+        // tft.print(" ");
+
+        tft.setCursor(50, 40); 
+        tft.printf("encode = %d     ", newPos);
         pos = newPos;
 
         Serial.printf("Direction = %d\n", encoder.getDirection());
+    }
+
+    if (digitalRead(ENCODER_KEY) == LOW) 
+    {
+        tft.setCursor(50, 70); 
+        tft.printf("encode btn press");
+    } 
+    else if (digitalRead(ENCODER_KEY) == HIGH) 
+    { 
+        tft.setCursor(50, 70); 
+        tft.printf("encode btn release");
+    }
+
+    if (digitalRead(BOARD_USER_KEY) == LOW) 
+    {
+        tft.setCursor(50, 100); 
+        tft.printf("user btn press");
+    } 
+    else if (digitalRead(BOARD_USER_KEY) == HIGH) 
+    { 
+        tft.setCursor(50, 100); 
+        tft.printf("user btn release");
     }
     delay(1);
 }
