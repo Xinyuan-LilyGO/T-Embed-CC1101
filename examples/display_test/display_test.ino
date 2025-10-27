ab9984e7d78b2f7e1982b4cbf0000f3bdefe13a7
ShallowGreen123 <2608653986@qq.com>
2025-10-27 14:37:01 +0800

    Delete redundant files

--- examples/display_test/display_test.ino
+++ examples/display_test/display_test.ino
@@ -1,13 +1,28 @@
 #include "TFT_eSPI.h" 
 
+// LORA
+#define BOARD_LORA_CS   12
+// TF card
+#define BOARD_SD_CS   13
+
 // About LCD definition in the file: lib/TFT_eSPI/User_Setups/Setup214_LilyGo_T_Embed_PN532.h
 TFT_eSPI tft;
 
 void setup(void)
 {
+    // LORA、SD and LCD use the same spi, in order to avoid mutual influence; 
+    // before powering on, all CS signals should be pulled high and in an unselected state;
+    pinMode(TFT_CS, OUTPUT);
+    digitalWrite(TFT_CS, HIGH);
+    pinMode(BOARD_SD_CS, OUTPUT);
+    digitalWrite(BOARD_SD_CS, HIGH);
+    pinMode(BOARD_LORA_CS, OUTPUT);
+    digitalWrite(BOARD_LORA_CS, HIGH);
+    
     tft.begin();
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
+
 }
 
 void loop(void)
--- firmware/examples/readme.md
+++ firmware/examples/readme.md
@@ -1,9 +1,14 @@
 
+---
 Test program for the nrf2401 module, Working frequency: 2400 MHz
 
+When an error occurs, open the serial port to check the error code and provide feedback.
 
 `nrf2401_send.bin`: When the nrf2401 is successfully initialized, it will continuously send data and the LED will start to flicker. 
 
 `nrf2401_recv.bin`: When the nrf2401 is successfully initialized, it waits to receive data. Once data is received, the LED starts to flash.
+
+
+--- 
 
 `test_display.bin`: Screen test program, used to check if the screen is functioning properly
