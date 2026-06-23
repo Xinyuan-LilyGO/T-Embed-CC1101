97cddb670cbbf33a77fa3f8e1c4bbdd9ba3f509c
ShallowGreen123 <2608653986@qq.com>
2026-06-23 17:21:32 +0800

    Rename the file

---
+++ lib/BQ27220/bq27220.h
@@ -0,0 +1,219 @@
+
+
+#ifndef BQ27227_H
+#define BQ27227_H
+
+#include "Arduino.h"
+#include <Wire.h>
+#include "bq27220_def.h"
+#include "bq27220_data_memory.h"
+
+#define DEFAULT_SCL  40
+#define DEFAULT_SDA  39
+
+typedef union BQ27220ControlStatusTag{
+    struct __reg
+    {
+        // Low byte, Low bit first
+        uint8_t BATT_ID : 3; /**< Battery Identification */
+        bool SNOOZE     : 1; /**< SNOOZE mode is enabled */
+        bool BCA        : 1; /**< fuel gauge board calibration routine is active */
+        bool CCA        : 1; /**< Coulomb Counter Calibration routine is active */
+        uint8_t RSVD0   : 2; /**< Reserved */
+        // High byte, Low bit first
+        uint8_t RSVD1; /**< Reserved */
+    } reg;
+    uint16_t full;
+} BQ27220ControlStatus;
+
+typedef union BQ27220BatteryStatusTag {
+    struct __reg
+    {
+        // Low byte, Low bit first
+        uint16_t DSG        : 1; /**< The device is in DISCHARGE */
+        uint16_t SYSDWN     : 1; /**< System down bit indicating the system should shut down */
+        uint16_t TDA        : 1; /**< Terminate Discharge Alarm */
+        uint16_t BATTPRES   : 1; /**< Battery Present detected */
+        uint16_t AUTH_GD    : 1; /**< Detect inserted battery */
+        uint16_t OCVGD      : 1; /**< Good OCV measurement taken */
+        uint16_t TCA        : 1; /**< Terminate Charge Alarm */
+        uint16_t RSVD       : 1; /**< Reserved */
+        // High byte, Low bit first
+        uint16_t CHGING     : 1; /**< Charge inhibit */
+        uint16_t FC         : 1; /**< Full-charged is detected */
+        uint16_t OTD        : 1; /**< Overtemperature in discharge condition is detected */
+        uint16_t OTC        : 1; /**< Overtemperature in charge condition is detected */
+        uint16_t SLEEP      : 1; /**< Device is operating in SLEEP mode when set */
+        uint16_t OCVFALL    : 1; /**< Status bit indicating that the OCV reading failed due to current */
+        uint16_t OCVCOMP    : 1; /**< An OCV measurement update is complete */
+        uint16_t FD         : 1; /**< Full-discharge is detected */
+    } reg;
+    uint16_t full;
+}BQ27220BatteryStatus;
+
+typedef enum {
+    Bq27220OperationStatusSecSealed = 0b11,
+    Bq27220OperationStatusSecUnsealed = 0b10,
+    Bq27220OperationStatusSecFull = 0b01,
+} Bq27220OperationStatusSec;
+
+typedef union BQ27220OperationStatusTag{
+    struct __reg
+    {
+        // Low byte, Low bit first
+        bool CALMD      : 1; /**< Calibration mode enabled */
+        uint8_t SEC     : 2; /**< Current security access */
+        bool EDV2       : 1; /**< EDV2 threshold exceeded */
+        bool VDQ        : 1; /**< Indicates if Current discharge cycle is NOT qualified or qualified for an FCC updated */
+        bool INITCOMP   : 1; /**< gauge initialization is complete */
+        bool SMTH       : 1; /**< RemainingCapacity is scaled by smooth engine */
+        bool BTPINT     : 1; /**< BTP threshold has been crossed */
+        // High byte, Low bit first
+        uint8_t RSVD1   : 2; /**< Reserved */
+        bool CFGUPDATE  : 1; /**< Gauge is in CONFIG UPDATE mode */
+        uint8_t RSVD0   : 5; /**< Reserved */
+    } reg;
+    uint16_t full;
+} BQ27220OperationStatus;
+
+typedef union BQ27220GaugingStatusTag{
+    struct __reg
+    {
+        // Low byte, Low bit first
+        bool FD       : 1; /**< Full Discharge */
+        bool FC       : 1; /**< Full Charge */
+        bool TD       : 1; /**< Terminate Discharge */
+        bool TC       : 1; /**< Terminate Charge */
+        bool RSVD0    : 1; /**< Reserved */
+        bool EDV      : 1; /**< Cell voltage is above or below EDV0 threshold */
+        bool DSG      : 1; /**< DISCHARGE or RELAXATION */
+        bool CF       : 1; /**< Battery conditioning is needed */
+        // High byte, Low bit first
+        uint8_t RSVD1 : 2; /**< Reserved */
+        bool FCCX     : 1; /**< fcc1hz clock going into CC: 0 = 1 Hz, 1 = 16 Hz*/
+        uint8_t RSVD2 : 2; /**< Reserved */
+        bool EDV1     : 1; /**< Cell voltage is above or below EDV1 threshold */
+        bool EDV2     : 1; /**< Cell voltage is above or below EDV2 threshold */
+        bool VDQ      : 1; /**< Charge cycle FCC update qualification */
+    } reg;
+    uint16_t full;
+} BQ27220GaugingStatus;
+
+class BQ27220{
+public:
+    BQ27220() : addr{BQ27220_I2C_ADDRESS}, wire(&Wire), scl(DEFAULT_SCL), sda(DEFAULT_SDA)
+    {}
+
+    bool begin()
+    {
+        return wire->begin(DEFAULT_SDA, DEFAULT_SCL);
+    }
+
+    bool getIsCharging(void){
+        BQ27220BatteryStatus batt;
+        if(getBatteryStatus(&batt)) {
+            return !batt.reg.DSG;
+        }
+        return false;
+    }
+
+    bool getCharingFinish(void)
+    {
+        BQ27220BatteryStatus batt;
+        getBatteryStatus(&batt);
+        if(!(batt.reg.DSG || getCurrent()) )
+            return true;
+        return false;
+    }
+
+    bool parameterCheck(uint16_t address, uint32_t value, size_t size, bool update);
+    bool dateMemoryCheck(const BQ27220DMData *data_memory, bool update);
+
+    bool init(const BQ27220DMData *data_memory = gauge_data_memory);
+    bool reset(void);
+    bool setDefaultCapacity(uint16_t cap);
+
+    // Sealed Access
+    bool sealAccess(void);
+    bool unsealAccess(void);
+    bool fullAccess(void);
+
+    // get
+    uint16_t getDeviceNumber(void);  // sub-commands
+    uint16_t getVoltage(void);
+    int16_t getCurrent(void);
+    int16_t getAverageCurrent(void);
+    bool getControlStatus(BQ27220ControlStatus *ctrl_sta);
+    bool getBatteryStatus(BQ27220BatteryStatus *batt_sta);
+    bool getOperationStatus(BQ27220OperationStatus *oper_sta);
+    bool getGaugingStatus(BQ27220GaugingStatus *gauging_sta);
+    uint16_t getTemperature(void);
+    uint16_t getFullChargeCapacity(void);
+    uint16_t getDesignCapacity(void);
+    uint16_t getRemainingCapacity(void);
+    uint16_t getStateOfCharge(void);
+    uint16_t getStateOfHealth(void);
+    uint16_t getChargeVoltageMax(void);
+
+    // i2c
+    uint16_t readRegU16(uint16_t reg) {
+        uint8_t data[2];
+        i2cReadBytes(reg, data, 2);
+        return ((uint16_t) data[1] << 8) | data[0];
+    }
+
+    // uint16_t readCtrlWord(uint16_t fun) {
+    //     uint8_t msb = (fun >> 8);
+    //     uint8_t lsb = (fun & 0x00FF);
+    //     uint8_t cmd[2] = { lsb, msb };
+    //     uint8_t data[2] = {0};
+
+    //     i2cWriteBytes((uint8_t)BQ27220_COMMAND_CONTROL, cmd, 2);
+
+    //     if (i2cReadBytes((uint8_t) 0, data, 2)) {
+    //         return ((uint16_t)data[1] << 8) | data[0];
+    //     }
+    //     return 0;
+    // }
+
+    bool controlSubCmd(uint16_t sub_cmd)
+    {
+        uint8_t msb = (sub_cmd >> 8);
+        uint8_t lsb = (sub_cmd & 0x00FF);
+        uint8_t buf[2] = { lsb, msb };
+        i2cWriteBytes(CommandControl, buf, 2);
+        return true;
+    }
+
+    bool i2cReadBytes(uint8_t reg, uint8_t * dest, uint8_t count) {
+        wire->beginTransmission(addr);
+        wire->write(reg);
+        wire->endTransmission(true);
+
+        wire->requestFrom(addr, count);
+        for(int i = 0; i < count; i++) {
+            dest[i] = wire->read();
+        }
+        return true;
+    }
+
+    bool i2cWriteBytes(uint8_t reg, uint8_t * src, uint8_t count) {
+        wire->beginTransmission(addr);
+        wire->write(reg);
+        for(int i = 0; i < count; i++) {
+            wire->write(src[i]);
+        }
+        wire->endTransmission(true);
+        return true;
+    }
+
+private:
+    TwoWire *wire = NULL;
+    uint8_t addr = 0;
+    int scl = -1;
+    int sda = -1;
+    BQ27220BatteryStatus bat_st;
+};
+
+#endif
+
---
+++ lib/BQ27220/bq27220_def.h
@@ -0,0 +1,83 @@
+
+
+#pragma once
+
+// device addr
+#define BQ27220_I2C_ADDRESS 0x55
+
+// device id
+#define BQ27220_DEVICE_ID 0x0220
+
+// command
+#define CommandControl             (0x00u)
+#define CommandAtRate              (0x02u)
+#define CommandAtRateTimeToEmpty   (0x04u)
+#define CommandTemperature         (0x06u)
+#define CommandVoltage             (0x08u)
+#define CommandBatteryStatus       (0x0Au)
+#define CommandCurrent             (0x0Cu)
+#define CommandRemainingCapacity   (0x10u)
+#define CommandFullChargeCapacity  (0x12u)
+#define CommandAverageCurrent      (0x14u)
+#define CommandTimeToEmpty         (0x16u)
+#define CommandTimeToFull          (0x18u)
+#define CommandStandbyCurrent      (0x1Au)
+#define CommandStandbyTimeToEmpty  (0x1Cu)
+#define CommandMaxLoadCurrent      (0x1Eu)
+#define CommandMaxLoadTimeToEmpty  (0x20u)
+#define CommandRawCoulombCount     (0x22u)
+#define CommandAveragePower        (0x24u)
+#define CommandInternalTemperature (0x28u)
+#define CommandCycleCount          (0x2Au)
+#define CommandStateOfCharge       (0x2Cu)
+#define CommandStateOfHealth       (0x2Eu)
+#define CommandChargeVoltage       (0x30u)
+#define CommandChargeCurrent       (0x32u)
+#define CommandBTPDischargeSet     (0x34u)
+#define CommandBTPChargeSet        (0x36u)
+#define CommandOperationStatus     (0x3Au)
+#define CommandDesignCapacity      (0x3Cu)
+#define CommandSelectSubclass      (0x3Eu)
+#define CommandMACData             (0x40u)
+#define CommandMACDataSum          (0x60u)
+#define CommandMACDataLen          (0x61u)
+#define CommandAnalogCount         (0x79u)
+#define CommandRawCurrent          (0x7Au)
+#define CommandRawVoltage          (0x7Cu)
+#define CommandRawIntTemp          (0x7Eu)
+
+// sub-command of CommandControl
+#define Control_CONTROL_STATUS         (0x0000u)
+#define Control_DEVICE_NUMBER          (0x0001u)
+#define Control_FW_VERSION             (0x0002u)
+#define Control_HW_VERSION             (0x0003u)
+#define Control_BOARD_OFFSET           (0x0009u)
+#define Control_CC_OFFSET              (0x000Au)
+#define Control_CC_OFFSET_SAVE         (0x000Bu)
+#define Control_OCV_CMD                (0x000Cu)
+#define Control_BAT_INSERT             (0x000Du)
+#define Control_BAT_REMOVE             (0x000Eu)
+#define Control_SET_SNOOZE             (0x0013u)
+#define Control_CLEAR_SNOOZE           (0x0014u)
+#define Control_SET_PROFILE_1          (0x0015u)
+#define Control_SET_PROFILE_2          (0x0016u)
+#define Control_SET_PROFILE_3          (0x0017u)
+#define Control_SET_PROFILE_4          (0x0018u)
+#define Control_SET_PROFILE_5          (0x0019u)
+#define Control_SET_PROFILE_6          (0x001Au)
+#define Control_CAL_TOGGLE             (0x002Du)
+#define Control_SEALED                 (0x0030u)
+#define Control_RESET                  (0x0041u)
+#define Control_OERATION_STATUS        (0x0054u)
+#define Control_GAUGING_STATUS         (0x0056u)
+#define Control_EXIT_CAL               (0x0080u)
+#define Control_ENTER_CAL              (0x0081u)
+#define Control_ENTER_CFG_UPDATE       (0x0090u)
+#define Control_EXIT_CFG_UPDATE_REINIT (0x0091u)
+#define Control_EXIT_CFG_UPDATE        (0x0092u)
+#define Control_RETURN_TO_ROM          (0x0F00u)
+
+#define UnsealKey1 (0x0414u)
+#define UnsealKey2 (0x3672u)
+
+#define FullAccessKey (0xffffu)
