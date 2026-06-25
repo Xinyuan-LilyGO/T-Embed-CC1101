060560cdb4bb83d42decabb211b628941274ddf1
ShallowGreen123 <2608653986@qq.com>
2026-06-25 16:41:23 +0800

    [Version]：V1.0.0 --- Add a new UI factory program

--- examples/factory/factory_build_info_autogen.cpp
+++ examples/factory/factory_build_info_autogen.cpp
@@ -1,5 +1,5 @@
 #include "factory_build_info.h"
 
 const char kFactorySoftwareVersion[] = "v1.0.0";
-const char kFactoryGitHash[] = "c4a522d1-dirty";
-const char kFactoryBuildStamp[] = "2026-06-25 16:26:33";
+const char kFactoryGitHash[] = "29975366-dirty";
+const char kFactoryBuildStamp[] = "2026-06-25 16:39:20";
